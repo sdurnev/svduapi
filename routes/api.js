1e031743d4d14d8d1314de8ca1e9799038235d32
svdu <sdurnev@gmail.com>
2021-09-03 18:02:20 +0600

    Commit 2

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/getip', function(req, res, next) {
+  ip=req.ip
+  if (ip.substr(0, 7) === "::ffff:") {
+    ip = ip.substr(7)
+  }
+  console.log(ip);
+  res.send('{ip:'+ip+'}');
+});
+
 module.exports = router;
